6b94f90b5635904ec282d8df46c8a34031144c40
HarshitSohaney <73911621+HarshitSohaney@users.noreply.github.com>
2022-04-14 17:35:55 -0400

    Added polling function

--- src/blockStackFunctions.h
+++ src/blockStackFunctions.h
@@ -1,6 +1,16 @@
+/* Header files for Block Stack game 
+	create by Harshit Sohaney & Yunjia Hao
+*/
 
+/* clear screen to background */
 void clear_screen();
+/* count down the VGA to synchronize buffers */
 void wait_for_vsync();
+/* draw boxes */
 void draw_boxes(int x1, int x2, int y1, int erase);
+/* display game over screen */
 void display_game_over();
+/* display home screen */
 void display_home();
+/* polling to start game */
+void start_game();
